7115f7a9c53458776eb4a205cbae0ae98a5e6a1b
Yury Amonsky <yury.amonsky@yandex.ru>
2022-08-15 00:32:47 +0300

    feat: импортировать в компонент Main экземпляр Api из модуля, добавить переменные состояния для профиля и вызвать эффект с получением данных пользователя

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import {api} from '../utils/Api';
 
 function Main(props){
+
+  const [userName, setUserName] = React.useState('');
+  const [userDescription, setUserDescription] = React.useState('');
+  const [userAvatar, setUserAvatar] = React.useState('');
+  //const [userInfo, setUserInfoObj] = React.useState('');
+  React.useEffect(()=>{
+    api.getUserInfo()
+      .then(res=>{
+        setUserName(res.name);
+        setUserDescription(res.about);
+        setUserAvatar(res.avatar);
+      })
+  },[userName,userDescription,userAvatar]);
+
   return(
     <main className="content">
       <section className="profile">
         <div className="profile__avatar-container" onClick={props.onEditAvatar}>
-          <img className="profile__avatar" src="#" alt="аватар" />
+          <img className="profile__avatar" src={userAvatar} alt="аватар" />
         </div>  
         <div className="profile__info">
-          <h1 className="profile__name"></h1>
+          <h1 className="profile__name">{userName}</h1>
           <button className="profile__edit-button" type="button" onClick={props.onEditProfile}></button>
-          <p className="profile__about-me"></p>
+          <p className="profile__about-me">{userDescription}</p>
         </div>
         <button className="profile__add-button" type="button" onClick={props.onNewLocation}></button>
       </section>
