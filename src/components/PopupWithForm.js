93d9c6a10bca71059e9a82818e9e9b79603ed2d5
Yury Amonsky <yury.amonsky@yandex.ru>
2022-08-18 20:52:23 +0300

    fix: убрать у формы атрибут noValidate

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -4,7 +4,7 @@ function PopupWithForm({title, name, isOpen, onClose, buttonText, children}){
     <div className={isOpen ? `popup popup_type_${name} popup_opened`: `popup popup_type_${name}`}>
       <div className="popup__container">
         <button className="popup__close-icon" type="button" onClick={onClose}></button>
-        <form className={`dialog-form dialog-form_type_${name}`} name={name} noValidate>
+        <form className={`dialog-form dialog-form_type_${name}`} name={name} >
           <h2 className="dialog-form__title dialog-form__title_type_edit-profile">{title}</h2>
           {children}
           <button className="dialog-form__submit-button" type="submit" name="submitButton"
