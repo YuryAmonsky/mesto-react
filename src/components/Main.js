3ec4d8e067d509d189f1239e6c0c57f3ecc5620d
Yury Amonsky <yury.amonsky@yandex.ru>
2022-08-18 16:22:29 +0300

    refactor: перенести значения атрибута children между тегов компонента

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,40 +1,40 @@
 import React from 'react';
-import {api} from '../utils/Api';
+import { api } from '../utils/Api';
 import Card from './Card';
 
-function Main(props){
+function Main(props) {
 
   const [userName, setUserName] = React.useState('');
   const [userDescription, setUserDescription] = React.useState('');
   const [userAvatar, setUserAvatar] = React.useState('');
   const [cards, setCards] = React.useState([]);
   //const [userInfo, setUserInfoObj] = React.useState('');
-  
-  React.useEffect(()=>{
+
+  React.useEffect(() => {
     Promise.all([
       /**загрузка данных пользователя в профиль*/
-        api.getUserInfo(),
-        /*загрузка списка карточек*/
-        api.loadLocations()  
-      ])
-        .then((values) =>{          
-          setUserName(values[0].name);
-          setUserDescription(values[0].about);
-          setUserAvatar(values[0].avatar);          
-          setCards([...values[1]]); 
-        })
-        .catch((err) =>{
-          console.log(err.status);
-          alert(`Ошибка загрузки данных:\n ${err.status}\n ${err.text}`);
-        });    
+      api.getUserInfo(),
+      /*загрузка списка карточек*/
+      api.loadLocations()
+    ])
+      .then((values) => {
+        setUserName(values[0].name);
+        setUserDescription(values[0].about);
+        setUserAvatar(values[0].avatar);
+        setCards([...values[1]]);
+      })
+      .catch((err) => {
+        console.log(err.status);
+        alert(`Ошибка загрузки данных:\n ${err.status}\n ${err.text}`);
+      });
   }, []);
-  
-  return(
+
+  return (
     <main className="content">
       <section className="profile">
         <div className="profile__avatar-container" onClick={props.onEditAvatar}>
           <img className="profile__avatar" src={userAvatar} alt="аватар" />
-        </div>  
+        </div>
         <div className="profile__info">
           <h1 className="profile__name">{userName}</h1>
           <button className="profile__edit-button" type="button" onClick={props.onEditProfile}></button>
@@ -45,16 +45,16 @@ function Main(props){
       <section aria-label="locations">
         <ul className="location-list">
           {
-            cards.map((card)=>
+            cards.map((card) =>
               <li key={card._id} className="location">
-                <Card card={card} onCardClick={props.onCardClick}/>
-              </li>                             
+                <Card card={card} onCardClick={props.onCardClick} />
+              </li>
             )
-          }          
+          }
         </ul>
       </section>
     </main>
   );
-}  
+}
 
 export default Main;
