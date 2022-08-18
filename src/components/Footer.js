da3adb2859d24325fb6a612f9fc18c20db91cfc2
Yury Amonsky <yury.amonsky@yandex.ru>
2022-08-18 18:58:41 +0300

    refactor: обновлять год в footer динамически

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer(){
   return(
     <footer className="footer">
-      <p className="copyright">&copy; 2022 Mesto Russia</p>
+      <p className="copyright">&copy; {new Date().getFullYear()} Mesto Russia</p>
     </footer>
   );
 }
