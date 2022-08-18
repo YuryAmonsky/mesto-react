c31b710b6a5747e852532f152bcb9352cbd63df1
Yury Amonsky <yury.amonsky@yandex.ru>
2022-08-18 19:25:00 +0300

    refactor: изменить условие открытия попапа с просмотром картинки

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,26 +1,14 @@
 import React from 'react';
-function ImagePopup(props){
-  if(props.card != null){
-    return(
-      <div className="popup popup_type_view-image popup_opened">
-        <div className="popup__container">
-          <button className="popup__close-icon" type="button" onClick={props.onClose}></button>
-          <img className="original-image" src={props.card.link} alt={`Фотография места ${props.card.name}`} />
-          <h3 className="image-caption">{props.card.name}</h3>
-        </div>
+function ImagePopup(props){ 
+  return(
+    <div className={`popup popup_type_view-image ${props.card? 'popup_opened':''}`}>
+      <div className="popup__container">
+        <button className="popup__close-icon" type="button" onClick={props.onClose}></button>
+        <img className="original-image" src={props.card?.link} alt={`Фотография места ${props.card?.name}`} />
+        <h3 className="image-caption">{props.card?.name}</h3>
       </div>
-    );
-  }else{
-    return(
-      <div className='popup popup_type_view-image'>
-        <div className="popup__container">
-          <button className="popup__close-icon" type="button" onClick={props.onClose}></button>
-          <img className="original-image" src="#" alt="Фотография места" />
-          <h3 className="image-caption">Название картинки</h3>
-        </div>
-      </div>
-    );
-  }        
+    </div>
+  );  
 }
 
 export default ImagePopup;
